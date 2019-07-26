0754cf2929e4182bd97cd2d89d8a2d76ef08b379
Yesid Hoyos <yesidfernandohoyos@gmail.com>
2019-07-25 20:34:55 -0500

    Se construye el proyecto con arquitectura hexagonal

---
+++ src/main/java/com/yesid/mudanza/MudanzaApplication.java
@@ -0,0 +1,13 @@
+package com.yesid.mudanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudanzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudanzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yesid/mudanza/MudanzaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yesid.mudanza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MudanzaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
